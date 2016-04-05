574924f7843457cd4855e6dd8b3116b15a6f3338
Jean Morrison <jeanm5@uw.edu>
2016-04-05 13:10:19 -0700

    long_profiles3

--- R/generate_data_wrappers.R
+++ R/generate_data_wrappers.R
@@ -92,19 +92,19 @@ generate_data_re <- function(){
 #'@title Wrapper to generate normal auto-regressive data with long profiles
 #'@description Generate normal AR  data. Run form a directory with a "data/" subdirectory.
 #'@export
-generate_data_long <- function(arsd, rho, n.rep, sample.size=c(20, 20)){
-  set.seed(2222222)
-  p <- dim(long_profiles2)[1]
-  K <- dim(long_profiles2)[2]
+generate_data_long <- function(sig, rho, re, n.rep, sample.size=c(10, 10), seed=NULL){
+  if(!is.null(seed))set.seed(seed)
+  p <- dim(long_profiles3)[1]
+  K <- dim(long_profiles3)[2]
 
   for(rep in 1:n.rep){
-    file.prefix <- paste0("ar_long_sd_", arsd, "_rho_", rho, "_n", rep)
+    file.prefix <- paste0("ar_long_sd_", sig, "_rho_", rho,"_re_", re,  "_n", rep)
     data.file <- paste0("data/", file.prefix, "_data.RData")
     y <- matrix(nrow=p, ncol=K)
     full.data <- matrix(nrow=p, ncol=0)
     for(i in 1:K){
       my.y <- replicate(n=sample.size[i],
-                expr=normal_data(long_profiles2[,i], arsd, rho, 0))
+                expr=normal_data(long_profiles3[,i], sig, rho, re))
       full.data <- cbind(full.data, my.y)
     }
     R <- list("Y"=full.data, "sample.size"=sample.size)
