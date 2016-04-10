d0141ecad22cef8753a20d2d13db10dfab995baf
Jean Morrison <jeanm5@uw.edu>
2016-04-09 19:54:21 -0700

    Fix axes on plotting function

--- R/rates_by_region.R
+++ R/rates_by_region.R
@@ -152,11 +152,13 @@ plot_rates2 <- function(rate.list, cols, main=""){
 
   whichCI <- seq(1, length(rate.list[[1]]$fct), length.out=11)
 
+  m <- max(rate.list[[1]]$fct)
+  for(i in 2:N) m <- max(m, rate.list[[i]]$fct)
   plotCI(x=rate.list[[1]]$fct[whichCI], y=rate.list[[1]]$tpr[whichCI],
-         uiw=rate.list[[1]]$s.e[whichCI], err="y", ylim=c(0, 1), xlim=c(0, 1),
+         uiw=rate.list[[1]]$s.e[whichCI], err="y", xlim=c(0, m), ylim=c(0, 1),
          pch=0, main=main, xlab="False Positive Count", ylab="TPR", cex.lab=1.5, col=cols[1], cex.main=2.5)
 
-  lines(rate.list[[1]]$fctr, rate.list[[1]]$tpr, lwd=1.5)
+  lines(rate.list[[1]]$fct, rate.list[[1]]$tpr, lwd=1.5)
   for(i in 2:N){
     plotCI(x=rate.list[[i]]$fct[whichCI], y=rate.list[[i]]$tpr[whichCI],
            uiw=rate.list[[i]]$s.e[whichCI], err="y", col=cols[i], add=TRUE, pch=0)
@@ -164,8 +166,6 @@ plot_rates2 <- function(rate.list, cols, main=""){
   }
   legend("bottomright", legend=rate.list$names, lty=1, col=cols)
 }
-
-
 
 
 get_regions <- function(x, min.length=3, merge.margin=0){
