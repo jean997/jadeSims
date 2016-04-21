a2e44510dbd24f8613e165499d2561c9645b1230
Jean Morrison <jeanm5@uw.edu>
2016-04-20 17:29:19 -0700

    update plot_rates2

--- R/plot_rates2.R
+++ R/plot_rates2.R
@@ -1,3 +1,12 @@
+#'@title Plot ROC curves for simulations
+#'@description Plot ROC curves for JADE and any other methods
+#'@param rate.list An object produced by
+#'@param cols Colors
+#'@param ltys Line types
+#'@param main Plot title
+#'@param lwd Line Width
+#'@return Nothing
+#'@export
 plot_rates2 <- function(rate.list, cols, ltys, main="", lwd=1.5){
   N <- length(rate.list)-1
 
@@ -10,7 +19,7 @@ plot_rates2 <- function(rate.list, cols, ltys, main="", lwd=1.5){
          pch=0, main=main, xlab="False Positive Count",
          ylab="TPR", cex.lab=1.5, col=cols[1], cex.main=2.5)
 
-  lines(rate.list[[1]]$fct, rate.list[[1]]$tpr, lwd=lwd, col=cols[1], lty=lty[1])
+  lines(rate.list[[1]]$fct, rate.list[[1]]$tpr, lwd=lwd, col=cols[1], lty=ltys[1])
   for(i in 2:N){
     plotCI(x=rate.list[[i]]$fct[whichCI], y=rate.list[[i]]$tpr[whichCI],
            uiw=rate.list[[i]]$s.e[whichCI], err="y", col=cols[i], add=TRUE, pch=0)
