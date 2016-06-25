495e0b1493afac2401dcf98f89e2974877efa096
Jean Morrison <jeanm5@uw.edu>
2016-06-25 16:27:35 -0700

    Also collect region level tpr and fct

--- R/aggregate_sims.R
+++ R/aggregate_sims.R
@@ -14,21 +14,34 @@
 #'  \item{\code{all.stats}}{B x n x F array of statistic values for alternative methods.}
 #'   \item{\code{site.labels}}{Which sites are separated}
 #'   \item{\code{tol, level}}{Parameters passed in}
-#'   \item{\code{avg.tpr, avg.fpr}}{Average true and false positive rates at given level and for JADE (14th column)}
+#'   \item{\code{avg.tpr, avg.fpr}}{Average true and false positive rates at given level and for JADE (last column)}
 #' }
 #'@export
-aggregate_sims <- function(file.prefix, profiles, save.file=NULL,
-                                 which.reps=1:60, tol=5e-3, level=0.1,
-                                 run.cv=TRUE, clean.jade=FALSE, use.cv=TRUE){
+aggregate_sims <- function(file.prefix, profiles, which.alt, stat.thresh,
+                           save.file=NULL,
+                            which.reps=1:100, tol=5e-3,
+                            run.cv=TRUE, clean.jade=FALSE, use.cv=TRUE,
+                            merge.margin=0, min.length=1, min.acc=0, max.prop=0.5){
+  stopifnot(length(which.alt) == length(stat.thresh))
+  n.alt = length(which.alt)
   n.sims <- length(which.reps)
   p <- dim(profiles)[1]
   K <- dim(profiles)[2]
-  site.labels <- abs(profiles[,1]-profiles[,2]) > tol
+  site.labels <- abs(profiles[,1]-profiles[,2]) > 1e-6
 
   all.sep <- list()
+  all.stats <- array(dim=c(n.sims, p, n.alt))
+  all.tpr <- all.fpr <- matrix(nrow=n.sims, ncol=n.alt+1)
+  all.region.tpr <- all.region.fct <- matrix(nrow=n.sims, ncol=n.alt+1)
 
-  j <- 1
-  for(rep in which.reps){
+
+  fdr.cols <- which(which.alt %in% c("mk.agg.qval", "mk.ind.qval",
+                                     "bss.qval" ,"spline.slim", "locfit.slim","tt.slim"))
+  p.cols <- which(which.alt %in% c("mk.agg.pval", "mk.ind.pval", "bss.pval",
+                                   "spline.pvals", "locfit.pvals", "tt.pvals"))
+
+  for(j in 1:length(reps)){
+    rep <- which.reps[j]
     jade.cv.file <- paste0("cv/", file.prefix , "_n", rep , "_cv.RData")
     orig.path.file <- paste0("path/", file.prefix, "_n", rep, "_path.0.RData")
     path.file.list <- paste0("path/", file.prefix, "_n", rep, "_path.", 1:5, ".RData")
@@ -37,36 +50,28 @@ aggregate_sims <- function(file.prefix, profiles, save.file=NULL,
 
     #Pvals for alternatives
     stats <- getobj(alt.file)
-    if(j==1){
-      pnames <- names(stats)
-      all.stats <- array(dim=c(n.sims, p, length(pnames)))
-      all.tpr <- all.fpr <- matrix(nrow=n.sims, ncol=length(pnames)+1)
-    }
-    all.stats[j, , ] <- as.matrix(stats)
+    ix <- match(which.alt, names(stats))
+    all.stats[j, , ] <- as.matrix(stats)[, ix]
 
     #FPR and TPR
-    fdr.cols <- which(pnames %in% c("mk.agg.qval", "mk.ind.qval",
-                                    "bss.qval" ,
-                                    "spline.slim", "locfit.slim",
-                                    "tt.slim"))
-    p.cols <- which(pnames %in% c("mk.agg.pval", "mk.ind.pval", "bss.pval",
-                                  "spline.pvals", "locfit.pvals", "tt.pvals"))
-    stat.cols <- which(pnames %in% c("bss.tstat", "spline.wt.ttests", " locfit.wt.ttests"))
 
-    for(i in 1:length(pnames)){
+    #stat.cols <- which(pnames %in% c("bss.tstat", "spline.wt.ttests", " locfit.wt.ttests"))
+
+    for(i in 1:length(which.alt)){
+      if(is.na(stat.thresh[i])) next
       my.labs <- rep(0, p)
-      if(i %in% fdr.cols | i %in% p.cols){
-        if(any(stats[,i] <=0)){
+      if((i %in% fdr.cols | i %in% p.cols) & any(stats[,ix[i]] <=0)){
           cat("Failed: ", pnames[i], "\n")
-        }else{
-          my.labs[stats[, i] < level] <- 1
-        }
+      }else{
+        my.labs[abs(stats[, ix[i]]) < stat.thresh[i]] <- 1
       }
       all.tpr[j, i] <- tpr.func(my.labs, site.labels)
       all.fpr[j, i] <- fpr.func(my.labs, site.labels)
+      rr <- rates_by_region(my.labs, site.labels)
+      all.region.tpr[j, i] <- rr["tpr"]
+      all.region.fct[j, i] <- rr["fp.ct"]
     }
-
-
+    i <- i  + 1
     #JADE
     path <- getobj(orig.path.file)
     u <- order(path$gammas[-1])
@@ -91,18 +96,24 @@ aggregate_sims <- function(file.prefix, profiles, save.file=NULL,
       }else{
         cv.obj <- getobj(jade.cv.file)
       }
-      all.tpr[j, length(pnames)+1] <- tpr.func(sep[, cv.obj$cv.1se.l1], site.labels)
-      all.fpr[j, length(pnames)+1] <- fpr.func(sep[, cv.obj$cv.1se.l1], site.labels)
+      my.labs <- sep[, cv.obj$cv.1se.l1]
+      all.tpr[j, i] <- tpr.func(my.labs, site.labels)
+      all.fpr[j, i] <- fpr.func(my.labs, site.labels)
+      rr <- rates_by_region(my.labs, site.labels)
+      all.region.tpr[j, i] <- rr["tpr"]
+      all.region.fct[j, i] <- rr["fp.ct"]
     }
-    j <- j+1
   }
 
 
   avg.tpr <- colMeans(all.tpr, na.rm=TRUE)
   avg.fpr <- colMeans(all.fpr, na.rm=TRUE)
+  avg.region.tpr <- colMeans(all.region.tpr, na.rm=TRUE)
+  avg.region.fct <- colMeans(all.region.fct, na.rm=TRUE)
   names <- c(pnames, "jade")
   R <- list("avg.tpr"=avg.tpr, "avg.fpr"=avg.fpr,
-            "names"=names, "all.sep"=all.sep, "tol"=tol, "level"=level,
+            "avg.region.tpr"=avg.region.tpr, "avg.region.fct"=avg.region.fct,
+            "names"=names, "all.sep"=all.sep, "tol"=tol, "stat.thresh"=stat.thresh,
             "all.stats"=all.stats, "site.labels"=site.labels)
   if(!is.null(save.file)) save(R, file=save.file)
   return(R)
